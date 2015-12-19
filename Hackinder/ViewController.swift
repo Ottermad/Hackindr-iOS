f3e1073dc3ee12ffa10ad58ffd164cf7da892db5
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-12-19 21:09:33 +0000

    Initial Commit

---
+++ Hackinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackinder
+//
+//  Created by Charlie Thomas on 19/12/2015.
+//  Copyright (c) 2015 Charlie Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
